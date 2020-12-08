6f136dcd75e8c7008d98016591c4cd5d9054c2e4
TheRawMeatball <therawmeatball@gmail.com>
2020-12-08 08:58:05 +0300

    Bugfix

--- server/queries/apply_deltas.sql
+++ server/queries/apply_deltas.sql
@@ -2,8 +2,8 @@ UPDATE
 	specific_homework
 SET
 	"delta_date" = now(),
-	"amount" = "amount" + "delta",
-	"delta" = 0
+	"progress" = "progress" + "delta",
+	"delta" = '0'
 WHERE
 	"delta_date" <= (now() - INTERVAL '32 hour')
 	AND "user_id" = $1;
